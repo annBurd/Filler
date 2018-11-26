ed273c43879c60fca9125da5ad0025db294877f4
aburdeni <>
2018-11-26 18:30:05 +0200

    normed

--- src/filler.c
+++ src/filler.c
@@ -6,13 +6,13 @@
 /*   By: aburdeni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/17 18:48:54 by aburdeni          #+#    #+#             */
-/*   Updated: 2018/11/26 17:39:30 by aburdeni         ###   ########.fr       */
+/*   Updated: 2018/11/26 18:27:35 by aburdeni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/filler.h"
 
-t_f			f;
+t_f			g_f;
 
 static char	**get_array(const char *s, size_t *n, size_t *x)
 {
@@ -37,24 +37,24 @@ static void	update_board(char *line)
 	size_t	i;
 
 	i = 0;
-	if (!f.board)
+	if (!g_f.board)
 	{
-		f.board = get_array(line, &f.n, &f.x);
-		while (i < f.n)
-			f.board[i++] = ft_strnew(f.x);
+		g_f.board = get_array(line, &g_f.n, &g_f.x);
+		while (i < g_f.n)
+			g_f.board[i++] = ft_strnew(g_f.x);
 	}
 	i = 0;
-	while (i < f.n && ft_getline(0, &line) > 0)
-		ft_strcpy(f.board[i++], line + 4);
+	while (i < g_f.n && ft_getline(0, &line) > 0)
+		ft_strcpy(g_f.board[i++], line + 4);
 }
 
 static void	update_token(char *line)
 {
 	size_t	i;
 
-	f.token = get_array(line, &f.tn, &f.tx);
+	g_f.token = get_array(line, &g_f.tn, &g_f.tx);
 	i = 0;
-	while (i < f.tn && ft_getline(0, &f.token[i]) > 0)
+	while (i < g_f.tn && ft_getline(0, &g_f.token[i]) > 0)
 		i++;
 }
 
@@ -62,13 +62,13 @@ int			main(void)
 {
 	static char	*line;
 
-	ft_bzero(&f, sizeof(t_f));
+	ft_bzero(&g_f, sizeof(t_f));
 	while (ft_getline(0, &line) > 0)
 	{
 		if (ft_strstr(line, "exec") && ft_strstr(line, "aburdeni.filler"))
 		{
-			f.player = (char)(ft_strstr(line, "p1") ? 'O' : 'X');
-			f.enemy = (char)(f.player == 'X' ? 'O' : 'X');
+			g_f.player = (char)(ft_strstr(line, "p1") ? 'O' : 'X');
+			g_f.enemy = (char)(g_f.player == 'X' ? 'O' : 'X');
 		}
 		else if (ft_strstr(line, "Plateau"))
 			update_board(line);
@@ -78,9 +78,9 @@ int			main(void)
 			search_place();
 		}
 	}
-	ft_arraystrfree(f.board);
-	if (f.token)
-		ft_arraystrfree(f.token);
+	ft_arraystrfree(g_f.board);
+	if (g_f.token)
+		ft_arraystrfree(g_f.token);
 	free(line);
 	return (0);
 }
